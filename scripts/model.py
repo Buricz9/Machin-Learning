5fc8c586d078e3a41949a8f8520b71658afce7a8
Buricz9 <buricz1906@gmail.com>
2024-12-16 00:35:18 +0100

    done

--- scripts/model.py
+++ scripts/model.py
@@ -1,32 +1,35 @@
 from sklearn.linear_model import LogisticRegression
-from sklearn.model_selection import GridSearchCV
-from sklearn.metrics import roc_auc_score
+from sklearn.model_selection import RandomizedSearchCV
+import numpy as np
 
-def train_logistic_regression_model_with_grid_search(X_train, y_train):
-    # Definiowanie przestrzeni przeszukiwania hiperparametrów
-    param_grid = {
-        'C': [0.0001, 0.001, 0.01, 0.1, 1, 10, 100],  # Różne wartości hiperparametru C
-        'solver': ['liblinear', 'lbfgs'],  # Algorytmy optymalizacji
-        'penalty': ['l1', 'l2']  # Rodzaje kar
+def optimize_logistic_regression_with_random_search(X_train, y_train):
+    # Definiowanie przestrzeni hiperparametrów dla RandomizedSearchCV
+    param_dist = {
+        'C': np.logspace(-4, 4, 20),  # Parametr regularyzacyjny C
+        'solver': ['liblinear', 'lbfgs'],  # Różne algorytmy optymalizacji
+        'penalty': ['l2']  # Tylko l2 dla 'lbfgs', 'liblinear' może obsługiwać 'l1'
     }
 
     # Inicjalizacja modelu Logistic Regression
     model = LogisticRegression(random_state=42, max_iter=1000)
 
-    # GridSearchCV do optymalizacji hiperparametrów
-    grid_search = GridSearchCV(
+    # RandomizedSearchCV dla optymalizacji hiperparametrów
+    random_search = RandomizedSearchCV(
         estimator=model,
-        param_grid=param_grid,
-        cv=5,  # Walidacja krzyżowa
-        scoring='roc_auc',  # Metryka optymalizacji
-        n_jobs=-1
+        param_distributions=param_dist,
+        n_iter=50,
+        cv=5,
+        n_jobs=-1,
+        verbose=0,
+        scoring='roc_auc',
+        random_state=42
     )
 
-    # Trening modelu z GridSearchCV
-    grid_search.fit(X_train, y_train)
+    # Trening modelu z RandomizedSearchCV
+    random_search.fit(X_train, y_train)
 
-    # Zwracanie najlepszego modelu i parametrów
-    best_model = grid_search.best_estimator_
-    best_params = grid_search.best_params_
+    # Wyciąganie najlepszego modelu i parametrów
+    best_model = random_search.best_estimator_
+    best_params = random_search.best_params_
 
     return best_model, best_params
