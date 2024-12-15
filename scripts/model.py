5f56d33d6808e923c0ea76f5d4c52a45fadd8958
Buricz9 <buricz1906@gmail.com>
2024-12-16 00:10:20 +0100

    1

--- scripts/model.py
+++ scripts/model.py
@@ -1,32 +1,15 @@
 from sklearn.ensemble import RandomForestClassifier
-from sklearn.model_selection import train_test_split, GridSearchCV
-from sklearn.preprocessing import StandardScaler
-from imblearn.over_sampling import SMOTE
+from sklearn.model_selection import GridSearchCV
+import numpy as np
 
-def train_random_forest_model(X, y):
-    # Zastosowanie SMOTE do zbalansowania klas
-    smote = SMOTE(random_state=42)
-    X_res, y_res = smote.fit_resample(X, y)
-    
-    # Podział danych na zbiór treningowy i testowy
-    X_train, X_test, y_train, y_test = train_test_split(X_res, y_res, test_size=0.2, random_state=42, stratify=y_res)
-
-    # Skalowanie danych
-    scaler = StandardScaler()
-    X_train = scaler.fit_transform(X_train)
-    X_test = scaler.transform(X_test)
-
-    # Trening modelu Random Forest
+def train_random_forest_model(X_train, y_train):
+    # Inicjalizacja modelu Random Forest
     model = RandomForestClassifier(random_state=42)
-    model.fit(X_train, y_train)
-    
-    return model, X_train, X_test, y_train, y_test
 
-def optimize_random_forest_model(model, X_train, y_train):
     # Definiowanie siatki parametrów do przeszukiwania dla GridSearchCV
     param_grid = {
         'n_estimators': [100, 200, 300],
-        'max_depth': [None, 10, 20, 30],
+        'max_depth': [10, 20, 30, None],
         'min_samples_split': [2, 5, 10],
         'min_samples_leaf': [1, 2, 4],
         'bootstrap': [True, False]
