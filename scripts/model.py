7ba51c3e03792af6bd646f658ed080abafc50ae8
Buricz9 <buricz1906@gmail.com>
2024-12-16 00:24:55 +0100

    5

--- scripts/model.py
+++ scripts/model.py
@@ -1,31 +1,34 @@
+import optuna
 from sklearn.svm import SVC
-from sklearn.model_selection import GridSearchCV
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import roc_auc_score
 
-def train_svm_model_with_grid_search(X_train, y_train):
-    # Definiowanie siatki parametrów do przeszukiwania dla GridSearchCV
-    param_grid = {
-        'C': [0.1, 1, 10, 100],
-        'gamma': [1, 0.1, 0.01, 0.001],
-        'kernel': ['rbf', 'linear']
-    }
+def train_svm_model_with_optuna(X_train, y_train):
+    # Definicja funkcji celu dla Optuna
+    def objective(trial):
+        # Proponowane wartości hiperparametrów
+        C = trial.suggest_loguniform('C', 1e-4, 1e2)
+        gamma = trial.suggest_loguniform('gamma', 1e-4, 1e1)
+        kernel = trial.suggest_categorical('kernel', ['rbf', 'linear'])
 
-    # Inicjalizacja modelu SVM
-    model = SVC(probability=True, random_state=42)
+        # Inicjalizacja modelu SVM
+        model = SVC(C=C, gamma=gamma, kernel=kernel, probability=True, random_state=42)
 
-    # Inicjalizacja GridSearchCV
-    grid_search = GridSearchCV(
-        estimator=model,
-        param_grid=param_grid,
-        cv=5,
-        n_jobs=-1,
-        verbose=1,
-        scoring='roc_auc'
-    )
+        # Trening modelu
+        model.fit(X_train, y_train)
 
-    # Trening modelu z GridSearchCV
-    grid_search.fit(X_train, y_train)
+        # Obliczanie AUC na zbiorze treningowym
+        auc = roc_auc_score(y_train, model.predict_proba(X_train)[:, 1])
+        return auc
 
-    # Zwrócenie najlepszego modelu i parametrów
-    best_model = grid_search.best_estimator_
-    best_params = grid_search.best_params_
+    # Tworzenie badania Optuna i optymalizacja
+    study = optuna.create_study(direction='maximize')
+    study.optimize(objective, n_trials=50, n_jobs=-1, timeout=600)
+
+    # Zwracanie najlepszego modelu i parametrów
+    best_trial = study.best_trial
+    best_params = best_trial.params
+    best_model = SVC(**best_params, probability=True, random_state=42)
+    best_model.fit(X_train, y_train)
+
     return best_model, best_params
