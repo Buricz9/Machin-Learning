b07bfaac68f677e4af21965a252671517ff2f545
Buricz9 <buricz1906@gmail.com>
2024-12-16 14:17:29 +0100

    d

--- scripts/model.py
+++ scripts/model.py
@@ -2,17 +2,17 @@ from sklearn.svm import SVC
 from sklearn.model_selection import GridSearchCV
 
 def train_svm_model_with_grid_search(X_train, y_train):
-    # Definiowanie siatki parametrów do przeszukiwania dla GridSearchCV
+    # Dyskretna przestrzeń hiperparametrów (zgodnie z ustalonym wzorcem)
     param_grid = {
-        'C': [0.1, 1, 10, 100],
-        'gamma': [1, 0.1, 0.01, 0.001],
-        'kernel': ['rbf', 'linear']
+        'C': [0.0001, 0.001, 0.01, 0.1, 1, 10, 100],  # Dyskretne wartości C
+        'gamma': [0.001, 0.01, 0.1, 1],               # Dyskretne wartości gamma
+        'kernel': ['rbf', 'linear']                   # Typ kernela
     }
 
     # Inicjalizacja modelu SVM
     model = SVC(probability=True, random_state=42)
 
-    # Inicjalizacja GridSearchCV
+    # GridSearchCV dla optymalizacji hiperparametrów
     grid_search = GridSearchCV(
         estimator=model,
         param_grid=param_grid,
