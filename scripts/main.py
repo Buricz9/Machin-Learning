68b06eaef2c7227f3a41a5fd22d74d44b3b9ee43
Buricz9 <buricz1906@gmail.com>
2024-12-16 13:09:21 +0100

    done

--- scripts/main.py
+++ scripts/main.py
@@ -8,27 +8,27 @@ from evaluation import evaluate_model, report_metrics, plot_confusion_matrix, pl
 # Ścieżka do pliku z danymi
 data_path = 'data/KAMC_COVID-19.csv'
 
-# Preprocessing and preparing data
+# Preprocessing i przygotowanie danych
 X, y = preprocess_data(data_path)
 X_train, X_test, y_train, y_test = prepare_data(X, y)
 
-# Optimizing and training the model
+# Optymalizacja i trening modelu LightGBM z RandomizedSearch
 best_model, best_params = optimize_lightgbm_with_randomsearch(X_train, y_train)
 print(f"Best parameters found: {best_params}")
 
-# Evaluating the model
+# Ewaluacja modelu
 accuracy, auc, f1, precision, recall = evaluate_model(best_model, X_test, y_test)
 report_metrics(accuracy, auc, f1, precision, recall)
 
-# Plotting evaluation metrics
+# Wykresy ewaluacji
 y_pred = best_model.predict(X_test)
 plot_confusion_matrix(y_test, y_pred)
 plot_roc_curve(best_model, X_test, y_test)
 
-# Pobranie nazw cech z danych
+# Nazwy cech
 nazwy_cech = X.columns.tolist()
 
-# Generowanie wykresów SHAP z polskimi nazwami cech
+# Generowanie wykresów SHAP
 print("Generowanie wykresów SHAP...")
 plot_shap_summary_polish(best_model, X_train, nazwy_cech)
 print("Wykres sumaryczny SHAP wygenerowany.")
